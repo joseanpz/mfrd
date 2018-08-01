2226bad64f83733334727c2dfbe60efbb4936af8
Jose Arce <joseangel.epzarce@gmail.com>
2018-08-01 13:04:10 -0500

    refactor fraud model

--- models/buro.py
+++ models/buro.py
@@ -1,35 +1,107 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from statsmodels import api as sm
+from statsmodels.stats.outliers_influence import OLSInfluence
 
 
-class InformacionBuroModel(object):
+class DataModel(object):
+    csv_file = None
+    columns = []
+    target = None
+    scaler = None
+    model_class = None
+    influence_class = None
+
+    def __init__(self, *args, **kwargs):
+        self.dataframe = pd.read_csv(self.csv_file, usecols=self.columns)
+        self._intecept = None
+        self._data = None
+        
+    def validate(self):
+        try:
+            self.dataframe = self.dataframe.append(self._data, ignore_index=True)
+        except Exception as e:
+            print(e)
+
+    def scaled_data(self):
+        return self.scaler.fit_transform(self.dataframe.drop(self.target, axis=1))
+
+    def get_results(self):
+        y = self.dataframe.loc[::, self.target]
+        X = self.scaled_data()
+        if self._intercept:
+            X = sm.add_constant(X)
+        results = self.model_class(y, X).fit()
+        # print(results.summary())
+        return results
+
+
+class FraudDataModel(DataModel):
     """
     Modelo de Infromación de buró usado para diagnosticar la posibilidad de presencia fraude en un cliente
     """
 
-    def __init__(self, CONSULTAS_U02M_SBCBR=None, MOP_MAXIMO_U24M_SERVSGRALES=None,
-                 PCT_DIR_JALGTO=None, PCT_APERTURAS_U18M=None, PCT_APERTURAS_ANT_REGBC=None,
-                 PCT_CTAS_BANCARIAS=None, PCT_CTAS_SERVS_GRALES=None, PROM_LIMCRE_MAXCRE_SERVSGRALES=None,
-                 PCT_ALERTAS_JUICIOS=None, UNIVERSO_11=None):
-        """
+    csv_file = 'JAT_FRAUDES.csv'
+    columns = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    target = 'UNIVERSO_11'
+    scaler = StandardScaler()
+    model_class = sm.OLS
+    influence_class = OLSInfluence
 
-        :param CONSULTAS_U02M_SBCBR:
-        :param MOP_MAXIMO_U24M_SERVSGRALES:
-        :param PCT_DIR_JALGTO:
-        :param PCT_APERTURAS_U18M:
-        :param PCT_APERTURAS_ANT_REGBC:
-        :param PCT_CTAS_BANCARIAS:
-        :param PCT_CTAS_SERVS_GRALES:
-        :param PROM_LIMCRE_MAXCRE_SERVSGRALES:
-        :param PCT_ALERTAS_JUICIOS:
-        :param UNIVERSO_11:
+    def __init__(self, data, intercept=True, *args, **kwargs):
         """
+        
+        :param data: 
+        :param args: 
+        :param kwargs: 
+        """
+        super(FraudDataModel, self).__init__(args, kwargs)
+        self._intercept = intercept
+        self._data = data
+        
+    def validate(self):
+        # TODO: validar _data
+        try:
+            self.dataframe.loc[self.dataframe["UNIVERSO_11"] == 2, 'UNIVERSO_11'] = 1
+            if self._data.get('MOP_MAXIMO_U24M_SERVSGRALES', -1) != -1:
+                self._data['MOP_MAXIMO_U24M_SERVSGRALES'] = 9 - self._data['MOP_MAXIMO_U24M_SERVSGRALES']
+            self.dataframe = self.dataframe.append(self._data, ignore_index=True)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-        self._CONSULTAS_U02M_SBCBR = CONSULTAS_U02M_SBCBR
-        self._MOP_MAXIMO_U24M_SERVSGRALES = MOP_MAXIMO_U24M_SERVSGRALES
-        self._PCT_DIR_JALGTO = PCT_DIR_JALGTO
-        self._PCT_APERTURAS_U18M = PCT_APERTURAS_U18M
-        self._PCT_APERTURAS_ANT_REGBC = PCT_APERTURAS_ANT_REGBC
-        self._PCT_CTAS_BANCARIAS = PCT_CTAS_BANCARIAS
-        self._PCT_CTAS_SERVS_GRALES = PCT_CTAS_SERVS_GRALES
-        self._PROM_LIMCRE_MAXCRE_SERVSGRALES = PROM_LIMCRE_MAXCRE_SERVSGRALES
-        self._PCT_ALERTAS_JUICIOS = PCT_ALERTAS_JUICIOS
-        self._UNIVERSO_11 = UNIVERSO_11
+    def impute(self):
+        self.dataframe.loc[self.dataframe["CONSULTAS_U02M_SBCBR"] == -1, 'CONSULTAS_U02M_SBCBR'] = np.NaN
+        self.dataframe.loc[self.dataframe["PCT_DIR_JALGTO"] == -1, 'PCT_DIR_JALGTO'] = np.NaN
+        self.dataframe.loc[self.dataframe["PCT_APERTURAS_U18M"] == -1, 'PCT_APERTURAS_U18M'] = np.NaN
+        self.dataframe.loc[self.dataframe["PCT_APERTURAS_ANT_REGBC"] == -1, 'PCT_APERTURAS_ANT_REGBC'] = np.NaN
+        self.dataframe.loc[self.dataframe["PCT_CTAS_BANCARIAS"] == -1, 'PCT_CTAS_BANCARIAS'] = np.NaN
+        self.dataframe.loc[self.dataframe["PCT_CTAS_SERVS_GRALES"] == -1, 'PCT_CTAS_SERVS_GRALES'] = np.NaN
+        self.dataframe.loc[self.dataframe["PROM_LIMCRE_MAXCRE_SERVSGRALES"] == -1, 'PROM_LIMCRE_MAXCRE_SERVSGRALES'] = np.NaN
+        self.dataframe.loc[self.dataframe["PCT_ALERTAS_JUICIOS"] == -1, 'PCT_ALERTAS_JUICIOS'] = np.NaN
+        self.dataframe.loc[self.dataframe["UNIVERSO_11"] == -1, 'UNIVERSO_11'] = np.NaN
+
+        # impute using the median on each column
+        self.dataframe = self.dataframe.fillna(self.dataframe.median())
+
+    def get_response(self, cutoff):
+        # imputamos
+        self.impute()
+
+        # influence object contains cooks distance
+        influence = self.influence_class(self.get_results())
+        a, b = influence.cooks_distance
+
+        # format response object
+        data = {
+            "fraud_risk": False,
+            "cook_value": a[-1]  # last item maps to input data
+        }
+        if data['cook_value'] > cutoff:
+            data["fraud_risk"] = True
+
+        return data
+
+
--- servicio_fraude.py
+++ servicio_fraude.py
@@ -5,78 +5,24 @@ from flask import Flask
 from flask import request
 from flask import json
 from flask import Response
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from statsmodels import api as sm
-from statsmodels.stats.outliers_influence import OLSInfluence
-from models.buro import InformacionBuroModel
+from models.buro import FraudDataModel
 
 app = Flask(__name__)
 logging_handler = RotatingFileHandler('logs/mfrd.log', maxBytes=10000, backupCount=1)
 logging_handler.setLevel(logging.INFO)
 app.logger.addHandler(logging_handler)
 
-
 CUTOFF = os.environ.get('CUTOFF', 0.000032891)
 
 
 @app.route('/', methods=['POST'])
-def verificacion_fraude():
-    print(request)
+def fraud_virification():
+
     app.logger.warning('A warning has occurred')
     app.logger.error('An error occurred')
     app.logger.info('Info')
 
-    #TODO: validar json de la peticion reasignar el parámetro NEW_OBS$MOP_MAXIMO_U24M_SERVSGRALES
-    #NEW_OBS$MOP_MAXIMO_U24M_SERVSGRALES = 9 - NEW_OBS$MOP_MAXIMO_U24M_SERVSG
-
-
-    jat_fraudes = pd.read_csv("JAT_FRAUDES.csv", usecols=[3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-
-    if request.json['MOP_MAXIMO_U24M_SERVSGRALES'] != -1:
-        request.json['MOP_MAXIMO_U24M_SERVSGRALES'] = 9 - request.json['MOP_MAXIMO_U24M_SERVSGRALES']
-
-    jat_fraudes = jat_fraudes.append(request.json, ignore_index=True)
-
-    jat_fraudes.loc[jat_fraudes["CONSULTAS_U02M_SBCBR"] == -1, 'CONSULTAS_U02M_SBCBR'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["PCT_DIR_JALGTO"] == -1, 'PCT_DIR_JALGTO'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["PCT_APERTURAS_U18M"] == -1, 'PCT_APERTURAS_U18M'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["PCT_APERTURAS_ANT_REGBC"] == -1, 'PCT_APERTURAS_ANT_REGBC'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["PCT_CTAS_BANCARIAS"] == -1, 'PCT_CTAS_BANCARIAS'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["PCT_CTAS_SERVS_GRALES"] == -1, 'PCT_CTAS_SERVS_GRALES'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["PROM_LIMCRE_MAXCRE_SERVSGRALES"] == -1, 'PROM_LIMCRE_MAXCRE_SERVSGRALES'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["PCT_ALERTAS_JUICIOS"] == -1, 'PCT_ALERTAS_JUICIOS'] = np.NaN
-    jat_fraudes.loc[jat_fraudes["UNIVERSO_11"] == -1, 'UNIVERSO_11'] = np.NaN
-
-    # imputacion a nulos o vacios por la mediana
-    jat_fraudes = jat_fraudes.fillna(jat_fraudes.median())
-
-    # homologar banderas fraude a 1
-    jat_fraudes.loc[jat_fraudes["UNIVERSO_11"] == 2, 'UNIVERSO_11'] = 1
-
-    # escalamos a dispersion normal de la muestra
-    scaler = StandardScaler()
-    scaled_params = scaler.fit_transform(jat_fraudes.drop('UNIVERSO_11', axis=1))
-
-    y = jat_fraudes.loc[::, 'UNIVERSO_11']
-    X = sm.add_constant(scaled_params)
-    model = sm.OLS(y, X)
-    results = model.fit()
-    print(results.summary())
-
-    influence = OLSInfluence(results)
-
-    # se calcula la distancia de cook
-    a, b = influence.cooks_distance
-
-    # summary = influence.summary_frame()
-    data = {
-        "riesgo_fraude": False,
-        "cook_value": a[-1]
-    }
-    if data['cook_value'] > CUTOFF:
-        data["riesgo_fraude"] = True
-
-    return Response(json.dumps(data), status=200, mimetype='application/json')
-
+    fraud_model = FraudDataModel(request.json)
+    if fraud_model.validate():
+        return Response(json.dumps(fraud_model.get_response(CUTOFF)), status=200, mimetype='application/json')
+    return Response(json.dumps({"Bad": "Request"}), status=400, mimetype='application/json')
